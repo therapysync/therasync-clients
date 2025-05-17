74b2e68863f38b1058f942eacfc7db2fc424a3bd
Ramal <ramalguseynov03@gmail.com>
2025-05-17 15:24:36 +0300

    рефакторинг

--- src/main/kotlin/project/therasync/service/UserServiceImpl.kt
+++ src/main/kotlin/project/therasync/service/UserServiceImpl.kt
@@ -1,44 +1,49 @@
 package project.therasync.service
 
-import org.springframework.stereotype.Service
-import project.therasync.data.model.User
-import project.therasync.data.repository.UserRepository
-import java.time.LocalDateTime
 import jakarta.persistence.EntityNotFoundException
+import org.springframework.stereotype.Service
 import project.therasync.data.dto.user.CreateUserRequest
 import project.therasync.data.dto.user.UpdateUserRequest
 import project.therasync.data.model.Role
+import project.therasync.data.model.User
+import project.therasync.data.repository.UserRepository
+import java.time.LocalDateTime
 
 @Service
 class UserService(
-    private val userRepository: UserRepository
+    private val userRepository: UserRepository,
 ) {
     fun createUser(request: CreateUserRequest): User {
-        val user = User(
-            id = request.id,
-            firstName = request.firstName,
-            lastName = request.lastName,
-            email = request.email,
-            role = Role.CLIENT,
-            createdAt = LocalDateTime.now(),
-            updatedAt = LocalDateTime.now()
-        )
+        val user =
+            User(
+                id = request.id,
+                firstName = request.firstName,
+                lastName = request.lastName,
+                email = request.email,
+                role = Role.CLIENT,
+                createdAt = LocalDateTime.now(),
+                updatedAt = LocalDateTime.now(),
+            )
         return userRepository.save(user)
     }
 
-    fun getUser(id: String): User {
-        return userRepository.findById(id)
+    fun getUser(id: String): User =
+        userRepository
+            .findById(id)
             .orElseThrow { EntityNotFoundException("User with ID $id not found") }
-    }
 
-    fun updateUser(id: String, request: UpdateUserRequest): User {
-        val user = getUser(id).copy(
-            firstName = request.firstName,
-            lastName = request.lastName,
-            email = request.email,
-            role = request.role,
-            updatedAt = LocalDateTime.now()
-        )
+    fun updateUser(
+        id: String,
+        request: UpdateUserRequest,
+    ): User {
+        val user =
+            getUser(id).copy(
+                firstName = request.firstName,
+                lastName = request.lastName,
+                email = request.email,
+                role = request.role,
+                updatedAt = LocalDateTime.now(),
+            )
         return userRepository.save(user)
     }
 
